be8eaab34f358e735888901e527d7e5a1835ba5e
xiften <108333567+not-ani@users.noreply.github.com>
2024-11-10 16:24:05 -0700

    fix: redirects for lessons

---
+++ src/app/(redirecting)/lesson/[id]/page.tsx
@@ -0,0 +1,54 @@
+//given a link to /lesson/[id] we will redirect the user to the actual lesson in the course
+import { redirect, notFound } from "next/navigation";
+import { hard_cache } from "@/lib/cache";
+import { eq } from "drizzle-orm";
+import { db } from "@/server/db";
+import { courses, lessons, units } from "@/server/db/schema";
+
+const getMinimalLesson = hard_cache(
+  async (id: string) => {
+    const lesson = await db.query.lessons.findFirst({
+      where: (lessons, { eq }) => eq(lessons.id, id),
+      columns: {
+        id: true,
+      },
+      with: {
+        unit: {
+          columns: {
+            id: true,
+          },
+          with: {
+            course: {
+              columns: {
+                id: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    return lesson;
+  },
+  ["lessonRedirect"],
+  {
+    revalidate: 60 * 60 * 24,
+  },
+);
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{
+    id: string;
+  }>;
+}) {
+  const id = (await params).id;
+  const lesson = await getMinimalLesson(id);
+  if (!lesson) {
+    console.log("Lesson not found");
+    return notFound();
+  }
+  return redirect(
+    `/course/${lesson.unit.course.id}/${lesson.unit.id}/${lesson.id}`,
+  );
+}
--- src/app/(redirecting)/lesson/page.tsx
+++
@@ -1,2 +0,0 @@
-//given a link to /lesson/[id] we will redirect the user to the actual lesson in the course
-export default function Page() {}
