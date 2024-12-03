4fe8f2962c32df50a5a13c41a8c6ddfbf56532b6
xiften <108333567+not-ani@users.noreply.github.com>
2024-12-02 18:24:46 -0700

    fix: moved db to Pool and WS

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,10 @@
-import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-import { neon } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 import { env } from "@/env";
 
 export const db = drizzle({
-  client: neon(env.DATABASE_URL),
+  client: new Pool({ connectionString: env.DATABASE_URL }),
   schema,
 });
