739ad94cdadecd8ea32e9468a2f1e8171f24de8d
ani <108333567+not-ani@users.noreply.github.com>
2025-01-15 20:13:21 -0700

    chore: added tags to hard caching

--- src/lib/cache/index.ts
+++ src/lib/cache/index.ts
@@ -5,7 +5,7 @@ import { unstable_cache as next_unstable_cache } from "next/cache";
 export const hard_cache = <Inputs extends unknown[], Output>(
   callback: (...args: Inputs) => Promise<Output>,
   key: string[],
-  options: { revalidate: number },
+  options: { revalidate?: number, tags?: string[] },
 ) => cache(next_unstable_cache(callback, key, options));
 
 export const unstable_cache = next_unstable_cache;
