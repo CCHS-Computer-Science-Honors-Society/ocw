0946d6b396953d4bdeedb9b32e8498a934c5fb4e
ani <108333567+not-ani@users.noreply.github.com>
2025-02-11 21:53:54 -0700

    chore: changed stale time to 5 minutes

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -10,7 +10,7 @@ export const createQueryClient = () =>
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: 30 * 1000,
+        staleTime: 60 * 1000 * 5 // 5 minutes
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
