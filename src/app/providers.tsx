268d46e7057c853fba8cdd62fb347394ba22841a
ani <108333567+not-ani@users.noreply.github.com>
2025-03-18 23:37:02 -0600

    feat: posthog for analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,19 @@ import { ExploreProvider } from "@/components/explore-button/context";
 import { TRPCReactProvider } from "@/trpc/react";
 import React from "react";
 import { Toaster } from "sonner";
+import { PostHogProvider } from "./_providers/posthog";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <TRPCReactProvider>
-        <ExploreProvider>{children}</ExploreProvider>
-      </TRPCReactProvider>
-      <Toaster position="top-center" richColors />
+      <PostHogProvider>
+        <TRPCReactProvider>
+          <ExploreProvider>
+            {children}
+          </ExploreProvider>
+        </TRPCReactProvider>
+        <Toaster position="top-center" richColors />
+      </PostHogProvider>
     </>
   );
 }
